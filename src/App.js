339646c8d143c823466bf6f4c5e634782d245158
guptamradul41-ops <guptamradul41@gmail.com>
2026-04-05 00:40:18 +0530

    admin dashboard

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import LoginPage from "./pages/LoginPage";
 import AdminLoginPage from "./pages/AdminLoginPage";
+import AdminDashboard from "./pages/admin_pages/AdminDashboard";
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
 import FloatingLoginButton from "./components/FloatingLoginButton";
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [theme, setTheme] = useState(() => {
     const saved = localStorage.getItem("theme");
-    // Default to dark neon if the user hasn’t chosen yet
     return saved === "dark" || saved === "light" ? saved : "dark";
   });
 
@@ -25,9 +23,11 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar theme={theme} setTheme={setTheme} />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+
         <Route
           path="/login"
           element={
@@ -38,19 +38,19 @@ function App() {
           }
         />
 
-        {/* ✅ Admin Login (ADD THIS) */}
         <Route
           path="/admin"
           element={
             <div>
               <AdminLoginPage />
-
             </div>
           }
         />
+
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
 }
-     
+
 export default App;
